1b570ca55ec67566db4046b1e882fa4aae152270
adol0214 <s910714tw@gmail.com>
2015-04-19 22:55:07 +0800

    補上註解
    註解忘記打了

--- Homework4.java
+++ Homework4.java
@@ -1,20 +1,24 @@
 class newMyStack extends ArrayList<Object>{
+	//判斷是不是空的
 	public boolean isEmpty(){
 		return super.isEmpty();
 	}
+	//把資料放進ArrayList
 	public void push(String o){
 		super.add(o);
 	}
+	//取得陣列大小
 	public int getSize(){
 		return super.size();
 	}
+	//傳回最上層的元素
 	public Object peek(){
 		return super.get(getSize() - 1);
 	}
+	//取出資料後刪除
 	public Object pop(){
 		Object A = super.get(getSize()-1);
 		super.remove(getSize()-1);
 		return A;
 	}
 }
-   
